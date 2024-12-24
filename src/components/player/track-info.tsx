d244b429de873cd4ff57c018dd8490218956e089
Nav Patel <navrpatel@gmail.com>
2024-12-24 13:11:37 -0500

    Top left fix

--- src/components/player/track-info.tsx
+++ src/components/player/track-info.tsx
@@ -1,8 +1,8 @@
 export function TrackInfo() {
   return (
-    <div className="flex justify-between items-center mt-2 text-wonamp-text-green font-mono text-sm">
+    <div className="flex flex-col items-end gap-1 mt-2 text-wonamp-text-green font-mono text-sm">
       <div className="text-xl tabular-nums hidden sm:block">00:04</div>
-      <div className="flex items-center gap-2">
+      <div className="flex flex-col items-end gap-1">
         <span className="bg-black px-1 border border-wonamp-border">192 kbps</span>
         <span className="bg-black px-1 border border-wonamp-border">44 khz</span>
       </div>
